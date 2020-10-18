fc2dacebf2b523ab11aa7a9442593361a7868f93
mobo <morgan.bohn@pc-scol.fr>
2020-10-18 16:58:56 +0200

    add a timer

--- PlayState.lua
+++ PlayState.lua
@@ -11,13 +11,19 @@ function mt:update(dt)
             item:update(dt)
         end
     end
+    -- Game lost if timer 0
+    if self.timer <= 0 then
+        GameState.setCurrent('Dead')
+    end
+    self.timer = self.timer - dt
 end
 
 function mt:draw()
     for _, item in ipairs(self.world.items) do
         item:draw()
     end
-    love.graphics.print({{0,0,0,0.5}, 'ROOM ' .. self.level_num .. '/' .. GAME_LEVEL_MAX}, 16, 16)
+    love.graphics.setNewFont(10)
+    love.graphics.print({{0,0,0,0.7}, 'ROOM ' .. self.level_num .. '/' .. GAME_LEVEL_MAX .. ' - TIME LEFT ' .. math.floor(self.timer) ..'s'}, 16, 16)
 end
 
 function mt:trigger(event, actor, data)
@@ -48,6 +54,7 @@ return {
       state.world = World.new()
       state.level = Level.new('maps/map' .. level_num, state)
       state.level_num = level_num
+      state.timer = 20 -- 20 secondes
       return state
     end
   }
